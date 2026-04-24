1bf906e57aa64dcbd78e36949ccb64657037b18b
Zidni Mubarock <zidmubarock@gmail.com>
2026-04-24 18:09:03 +0700

    chore: port design-system CSS into renderer

--- src/renderer/src/main.tsx
+++ src/renderer/src/main.tsx
@@ -1,4 +1,7 @@
 import { createRoot } from 'react-dom/client'
 import App from './App'
+import './styles/tokens.css'
+import './styles/components.css'
+import './styles/onboarding.css'
 
 createRoot(document.getElementById('root')!).render(<App />)
